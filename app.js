adf2bc6f1fc6779d0d49693f1d1d84c5cd911ac6
Amit Schandillia <amit@peppyburro.com>
2017-07-03 01:10:14 +0000

    Rolled back Webpack hot reloading

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const index = require('./routes/index');
 const api = require('./routes/api/index');
 const users = require('./routes/api/users');
@@ -24,7 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicons', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
