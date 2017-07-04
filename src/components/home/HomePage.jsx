111aaf4a6d6ff8b99fe8c679b0d030b675752421
Amit Schandillia <amit@peppyburro.com>
2017-07-04 14:22:32 +0000

    Changed text in HomePage.jsx

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -6,7 +6,7 @@ export default function HomePage() {
   return (
     <div className="row">
       <div className="col-sm-12 col-lg-8">
-        This is the home page.
+        This is the home page. Yayyyy!
       </div>
       <Sidebar />
     </div>
