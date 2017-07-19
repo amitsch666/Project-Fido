07437f295f23bcd17711821f7a5add5e88b7242b
Amit Schandillia <amit@peppyburro.com>
2017-07-19 05:35:09 +0000

    Tested API endpoint registration with secured password hashing

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const mongoose = require('mongoose');
 const index = require('./routes/index');
 const api = require('./routes/api/index');
 const users = require('./routes/api/users');
+const authentication = require('./routes/api/authentication');
 
 const app = express();
 
@@ -44,6 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', api);
 app.use('/api/users', users);
+app.use('/api/authentication', authentication);
 app.use('/*', index);
 
 // Configure Passport
